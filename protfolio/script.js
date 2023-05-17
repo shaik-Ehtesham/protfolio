b237db6bccfc2d5f496052f8a7625d3f9c451e60
Ehtesham ul haq <123198516+shaik-Ehtesham@users.noreply.github.com>
2023-05-17 14:45:47 +0530

    Add files via upload

---
+++ protfolio/script.js
@@ -0,0 +1,15 @@
+// Tuping Animation using typed.js
+var options={
+    strings: ["A Android Developer","A Programmer","An Engineer","Coding Enthusiasist"],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true
+}
+var typed=new Typed('.typing',options)
+
+
+// scroll top
+function scrolltop() {
+    let a = window.document;
+    a = location.href = '#hero-section';
+}
